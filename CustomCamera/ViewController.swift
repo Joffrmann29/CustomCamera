512eef54f93f714e567f214d5ae323caa9ab29ae
Mann, Joffery <Joffery.Mann@scientificgames.com>
2018-02-07 10:54:32 -0500

    Initial Commit

---
+++ CustomCamera/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CustomCamera
+//
+//  Created by Mann, Joffery on 2/7/18.
+//  Copyright © 2018 Mann, Joffery. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
